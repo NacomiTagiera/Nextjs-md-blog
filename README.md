f331c7a8c0bf63f0d598adc1b382017923198010
Kuba Pawlak <kubapawlak1051@wp.pl>
2023-03-15 20:06:24 +0100

    Initialized project using create-next-app, updated README

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Kocham Gry
 
-Responsive website about computer games. I'm working on this project to practice React Bootstrap and SASS. You can take a look at the previous, finished version of this webpage on the second branch.
+This will be a **Next.js MDX blog** about my favourite video games. Stay tuned, I will add more code soon!
+I encourage you to take a look at the commit history to see how I improved over the months. There are a lot of commits, because at the begging of my frontend journey I didn't know how to use GIT and GitHub properly, sorry for that :)
---
+++ kocham-gry/src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import "@/styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Fragment>
+      <Head>
+        <title>Kocham Gry</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="This is a blog about my favourite video games."
+        />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
+}
